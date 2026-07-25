c5ce8e673c4ad8e9525fef8771a7e5e41a644f18
Bernardinai <administracija@bernardinai.lt>
2026-07-25 08:43:28 +0300

    Update naujienlaiskis.py

--- naujienlaiskis.py
+++ naujienlaiskis.py
@@ -338,8 +338,9 @@ html_kodas += f"""
                     <p><strong>Rita Bagdonaitė</strong><br>Religijos redaktorė<br>rita.bagdonaite@bernardinai.lt</p>
                     <p><strong>Vytautas Markevičius</strong><br>Žurnalistas<br>vytautas.markevicius@bernardinai.lt</p>
                     <p><strong>Austina Pakalnytė</strong><br>Žurnalistė<br>austina.pakalnyte@bernardinai.lt</p>
-                    <p><strong>Tomas Kemzūra</strong><br>Žurnalistas<br>tomas.kemzura@bernardinai.lt</p>
-                    <p><strong>Laima Šiušaitė</strong><br>Kalbos redaktorė<br>laima.siusaite@bernardinai.lt</p>
+                    <p><strong>Ugnė Tulaitė</strong><br>Žurnalistė<br>ugne.tulaite@bernardinai.lt</p>
+                    <p><strong>Austėja Žalalytė</strong><br>Žurnalistė<br>austeja.zalalyte@bernardinai.lt</p>
+                    <p><strong>Laima Šiušaitė</strong><br>Dienos redaktorė<br>laima.siusaite@bernardinai.lt</p>
                 </td>
                 <td width="50%" valign="top" style="padding-left: 20px;">
                     <div style="font-size: 14pt; color: #111; font-weight: bold; margin-bottom: 15px; border-bottom: 1px solid #eee; padding-bottom: 5px;">Bendradarbiai</div>
@@ -347,7 +348,6 @@ html_kodas += f"""
                     <p><strong>Evgenia Levin</strong><br>Fotografė<br>el@zeneka.lt</p>
                     <p><strong>Rasa Baškienė</strong><br>Tekstų autorė<br>rasa@bernardinai.lt</p>
                     <p><strong>Gediminas Zelvaras</strong><br>Tekstų autorius<br>gediminaszelvaras22@gmail.com</p>
-                    <p><strong>Ugnė Tulaitė</strong><br>Tekstų autorė</p>
                     <p><strong>Saulena Žiugždaitė</strong><br>Tekstų autorė<br>saulena@bernardinai.lt</p>
                     <p><strong>Aurelija Plokštytė</strong><br>Tekstų autorė<br>aurelija.plokstyte@bernardinai.lt</p>
                     <p><strong>Teodoras Žukas</strong><br>Tekstų autorius<br>teodoras.zukas@gmail.com</p>
