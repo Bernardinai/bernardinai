14b4440eb05d6ed35938b9a1e7d82ff185ed2a8e
Bernardinai <administracija@bernardinai.lt>
2026-09-11 23:33:26 +0300

    Update naujienlaiskis.py

--- naujienlaiskis.py
+++ naujienlaiskis.py
@@ -491,7 +491,7 @@ html_kodas = f"""<!DOCTYPE html><html><head><meta charset="utf-8">
             <div class="meta-footer">
                 <div><strong>DATA:</strong> {leidinio_data} &nbsp;&nbsp;|&nbsp;&nbsp; <strong>NUMERIS:</strong> {leidinio_numeris}</div>
                 <div style="margin-top: 4px;"><strong>LAIKOTARPIS:</strong> {savaites_laikotarpis}</div>
-                <div style="margin-top: 10px; font-size: 8pt; opacity: 0.65; letter-spacing: 1.5px;">ISSN 3120-9696</div>
+                <div style="margin-top: 10px; font-size: 8.5pt; opacity: 0.75;">ISSN 3120-9696</div>
             </div>
         </div>
     </div>
